31677a367b6c60b94f208ce6def5722c94fe0115
Eliorco <eliorc1988@gmail.com>
2020-11-01 22:13:32 +0200

    second commit subproccess wrapper and two txt commands file

---
+++ README.md
@@ -0,0 +1,7 @@
+Easy mac Setup
+
+1. clone the repo
+2. run the code
+3. start developing
+
+* Mac os only (for now)
--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 import os
+import sys
+import subprocess
+
+
+def do_bash_command(command: str, is_output: bool) -> bool:
+    try:
+        process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+        output, err = process.communicate()
+    except subprocess.SubprocessError as se:
+        print(se, err)
+        return False
+    return str(output).encode("utf-8") if is_output else True
 
 
 def main():
+    with open('commands.txt', 'r') as f:
+        for command in f.readlines():
+            print(do_bash_command(command, True))
     return True
 
 
